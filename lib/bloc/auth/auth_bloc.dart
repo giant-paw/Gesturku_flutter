4f9cd266254a299d4115062eb8c65c2786d2c8d3
giant-paw <giantpawbusiness@gmail.com>
2025-07-01 09:14:31 +0700

    auth.bloc

--- lib/bloc/auth/auth_bloc.dart
+++ lib/bloc/auth/auth_bloc.dart
@@ -1,14 +1,43 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:gesturku_app/models/pengguna.dart';
+import '../../models/pengguna.dart';
+import '../../repositories/auth_repository.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final AuthRepository authRepository;
+
+  AuthBloc({required this.authRepository}) : super(AuthUninitialized()) {
+    on<AppStarted>(_onAppStarted);
+    on<LoggedIn>(_onLoggedIn);
+    on<LoggedOut>(_onLoggedOut);
+  }
+
+  void _onAppStarted(AppStarted event, Emitter<AuthState> emit) async {
+    final bool hasToken = await authRepository.hasToken();
+    if (hasToken) {
+      emit(AuthUnauthenticated());
+    } else {
+      emit(AuthUnauthenticated());
+    }
+  }
+
+  void _onLoggedIn(LoggedIn event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      final result = await authRepository.login(event.email, event.password);
+      final Pengguna pengguna = result['user'];
+      emit(AuthAuthenticated(pengguna: pengguna));
+    } catch (e) {
+      emit(AuthUnauthenticated());
+    }
+  }
+
+  void _onLoggedOut(LoggedOut event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    await authRepository.logout();
+    emit(AuthUnauthenticated());
   }
 }
