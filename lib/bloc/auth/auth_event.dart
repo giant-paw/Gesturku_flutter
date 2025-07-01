4ea33e84c72a210063eb703a1ce78acb69643135
giant-paw <giantpawbusiness@gmail.com>
2025-07-01 08:46:30 +0700

    membuat event log in dan log out

--- lib/bloc/auth/auth_event.dart
+++ lib/bloc/auth/auth_event.dart
@@ -6,3 +6,19 @@ sealed class AuthEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class AppStarted extends AuthEvent {}
+
+// Event jika login dipencet
+class LoggedIn extends AuthEvent {
+  final String email;
+  final String password;
+
+  const LoggedIn({required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+// Event logout di pencet
+class LoggedOut extends AuthEvent{}
